199b8412c93e2f6e5d52b11a37b06fdce08013a9
shaniqua simpson <niquacharisdesigns@gmail.com>
2019-03-13 14:58:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) { 
+    return "This one is on me!"
+  }
+
+ if (someValue >= 2000 && someValue < 2500) {
+   return "I will gladly take your thirty bucks."
+ }
+ 
+ else if (someValue >= 2500) {
+  return "No can do."
+  
+}
+
+
+}
+  //this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
